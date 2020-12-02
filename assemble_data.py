8ea0936756064c003279063024223c0d5dd86269
Daniel Hocevar <danielhocevar1@gmail.com>
2020-12-01 19:49:13 -0500

    Cleaned up data preprocessing pipeline. Data is ready to be analyzed.

--- assemble_data.py
+++ assemble_data.py
@@ -1,34 +1,80 @@
-"""Build the data sets from a variety of sources"""
+"""Build the data sets from a variety of sources.
+Additionally, provide functions that read the datasets from their csvs.
+
+Functions with prefix "assemble" build the datasets into pandas dataframes.
+Functions with prefix "make" convert dataframes to csvs.
+Functions with prefux "read" read csvs into dataframes.
+"""
 
 import requests
 import pandas as pd
 import json
 import os
 
-# 58526
 DELIMITER = '","'  # Since the government likes microsoft, we have to use weird delimiters
 DATA_DIRECTORY = "./data"
 
-geojson = f'{DATA_DIRECTORY}/Fire_Disturbance_Area.json'
+# Input files
+FIRE_AREA_INPUT = f'{DATA_DIRECTORY}/Fire_Disturbance_Area.geojson'
+FIRE_POINT_INPUT = f'{DATA_DIRECTORY}/fire_point.csv'
+WEATHER_DATA_INPUT = f'{DATA_DIRECTORY}/Station Inventory EN.csv'
+
+# Output files
+FIRE_AREA_OUTPUT =f'{DATA_DIRECTORY}/processed_data/Fire_Disturbance_Area_Clean.csv'
+FIRE_POINT_OUTPUT = f'{DATA_DIRECTORY}/processed_data/Fire_Disturbance_Point_Clean.csv'
+WEATHER_STATION_OUT = f'{DATA_DIRECTORY}/processed_data/weather_station_data.csv'
+
 
 def assemble_fire_disturbance_point() -> pd.DataFrame:
-    fires = pd.read_csv('fires_point.csv')
+    """
+    Save a new csv containing only fire disturbance point data for fires which
+    took place after the beginning of the year 1998
+
+    The return value of this function is stored at:
+    ./data/Fire_Disturbance_Point_Clean.csv
+    """
+    fires = pd.read_csv(FIRE_POINT_INPUT)
     fires = fires.rename(columns={'X': 'longitude', 'Y': 'latitude'})
+    remove_indicies = []
     for row, item in fires.iterrows():
         print(row)
         if int(item['FIRE_START_DATE'].split('/')[0]) < 1998:
-            fires.drop(row)
+            remove_indicies.append(row)
+    print('remove indicies')
+    for row in remove_indicies:
+        print(row)
+        fires = fires.drop(row)
+    print('Saving')
+    fires.to_csv(FIRE_POINT_OUTPUT)
+    return fires
+
+
+def read_fire_disturbance_point() -> pd.DataFrame:
+    """
+    Read the csv containing fire disturbance point data, and return a dataframe
+    representation of its content
+    """
+    fires = pd.read_csv(FIRE_POINT_OUTPUT)
     return fires
 
 
 def assemble_fire_disturbance_area() -> pd.DataFrame:
-    with open(geojson) as json_file:
+    """ Filter and build a dataset from the raw 'Fire_Disturbance_Area.geojson' file.
+
+    This function does not take any parameters because it is expected to be rewritten directly.
+
+    The function extracts the desired properties of area fires from the file, as well
+    as caculates the approximate location of a given area fire by averaging it's shape.
+    """
+    # Open the file
+    with open(FIRE_AREA_INPUT) as json_file:
         data = json.load(json_file)
-        
+
         properties_so_far = []
-        
+
+        # Select data row-by-row
         for row in data['features']:
-            if int(row['properties']['FIRE_START_DATE'].split('/')[0]) >= 1998:                
+            if int(row['properties']['FIRE_START_DATE'].split('/')[0]) >= 1998:
                 properties = [row['properties']['OGF_ID'],
                           row['properties']['FIRE_TYPE_CODE'],
                           row['properties']['FIRE_START_DATE'],
@@ -36,6 +82,7 @@ def assemble_fire_disturbance_area() -> pd.DataFrame:
                           row['properties']['FIRE_FINAL_SIZE']
                           ]
 
+                # Analyse and retrieve the shape of the fire
                 if row['geometry']['type'] == 'MultiPolygon':
                     collapsed = [coord for sub1 in row['geometry']['coordinates']
                                     for sub2 in sub1
@@ -44,32 +91,47 @@ def assemble_fire_disturbance_area() -> pd.DataFrame:
                 if row['geometry']['type'] == 'Polygon':
                     collapsed = [coord for sub1 in row['geometry']['coordinates']
                                     for coord in sub1]
+                # Average the shape of the fire into one coordinate
                 avg_long = sum([coord[0] for coord in collapsed]) / len(collapsed)
                 avg_lat = sum([coord[1] for coord in collapsed]) / len(collapsed)
-                properties.append(avg_long)   
+                properties.append(avg_long)
                 properties.append(avg_lat)
-                #print(collapsed)
-                
-            
+
+                # Append the properties for that row
                 properties_so_far.append(properties)
 
-        columnLabels = ['OGF_ID', 
-                        'FIRE_TYPE_CODE', 
-                        'FIRE_START_DATE', 
-                        'FIRE_GENERAL_CAUSE_CODE', 
+        columnLabels = ['OGF_ID',
+                        'FIRE_TYPE_CODE',
+                        'FIRE_START_DATE',
+                        'FIRE_GENERAL_CAUSE_CODE',
                         'FIRE_FINAL_SIZE',
                         'LONGITUDE',
                         'LATITUDE']
+        # Build the pandas dataframe from the extracted data, and the column labels
         return pd.DataFrame(properties_so_far, columns = columnLabels)
-    
-def make_fire_disturbance_area_csv():
+
+
+def make_fire_disturbance_area_csv() -> None:
+    """Assemble and save fire disturbance area data to a csv (makes for faster loading in the
+    future)
+    """
     data = assemble_fire_disturbance_area()
-    data.to_csv(f'{DATA_DIRECTORY}/processed_data/fire_disturbance_point.csv')
+    data.to_csv(FIRE_AREA_OUTPUT)
+
+
+def read_fire_disturbance_area() -> pd.DataFrame:
+    """
+    Return a pandas dataframe containing the data read from the fire disturbance area csv
+    """
+    fires = pd.read_csv(FIRE_AREA_OUTPUT)
+    return fires
 
 
 #  Assemble the weather data
 def filter_stations(min_year: int) -> pd.DataFrame:
-    unfiltered = pd.read_csv(f'{DATA_DIRECTORY}/Station Inventory EN Full.csv', header=[2])
+    """Return the rows of all weather stations that operate throughout years min_year and 2020
+    """
+    unfiltered = pd.read_csv(WEATHER_DATA_INPUT, header=[2])
 
     filtered_first_year = unfiltered[unfiltered['First Year'] <= min_year]
     filtered_last_year = filtered_first_year[unfiltered['Last Year'] == 2020]
@@ -78,30 +140,39 @@ def filter_stations(min_year: int) -> pd.DataFrame:
 
 
 def assemble_weather_data_by_station(stationID: int, min_year: int, max_year: int) -> pd.DataFrame:
+    """Query an api for weather data of a given stationID throughout the years min_year - max_year
+    """
     print(f'min_year: {min_year}, max_year: {max_year}')
-    data_frames = []
+    data_frames = []  # Accumulator
 
+    # The api must be queried separately for each year. Government tech at its finest.
     for year in range(min_year, max_year + 1):
         print(year)
         data = requests.get(f'https://climate.weather.gc.ca/climate_data/bulk_data_e.html? \
                               format=csv&stationID={stationID}&Year={year}&Month=1&\
                               Day=14&timeframe=2&submit= Download+Data')
 
+        # Convert the line endings characters to ones python-compatable
         fixed_data = data.text.replace('\r\n', '\n')
         fixed_data = fixed_data.splitlines()
 
+        # Balance each row so that it is at least the length of the header
         for row in range(len(fixed_data)):
             fixed_data[row] += DELIMITER * (len(fixed_data[0].split(DELIMITER)) -
                                             len(fixed_data[row].split(DELIMITER)))
 
+        # Build and append the dataframe to the accumulator
         data_frames.append(pd.DataFrame([x.split(DELIMITER) for x in fixed_data[1:]],
                                         columns=fixed_data[0].split(DELIMITER)))
 
+    # Combine all rows into one dataframe, and return it
     return pd.concat(data_frames)
 
 
 def make_relevant_weather_data(year: int = 1998) -> None:
-    stations = filter_stations(year)
+    """Produces csv files for every eligable weather station operating throught 1998 to 2020.
+    """
+    stations = filter_stations(year) # Get eligable stations
 
     for _, station in stations.iterrows():
         print(station)
@@ -109,20 +180,20 @@ def make_relevant_weather_data(year: int = 1998) -> None:
                                                 year, station['Last Year'])
 
         data.to_csv(f'{DATA_DIRECTORY}/weather_data/{station["Name"]}_{station["Station ID"]}.csv')
-        
-
-fire_point_data = assemble_fire_disturbance_point()
-fire_area_data = assemble_fire_disturbance_area()
 
 
 def make_big_weather_data():
-    data_frames = [pd.read_csv(f'{DATA_DIRECTORY}/weather_data/{file}') 
+    """Makes a giant csv file with all the weather station data.
+    Requires the existance of individual station data in {DATA_DIRECTORY}/weather_data/ in
+    the form of csv's.
+    """
+    data_frames = [pd.read_csv(f'{DATA_DIRECTORY}/weather_data/{file}')
                    for file in os.listdir(f'{DATA_DIRECTORY}/weather_data/') if '.csv' in file]
-    
-    return pd.concat(data_frames)
+
+    pd.concat(data_frames).to_csv(WEATHER_STATION_OUT)
 
 
 if __name__ == '__main__':
     # print(answer)
     pass
-    
+
---
+++ streamlit_main.py
@@ -0,0 +1,121 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from PIL import Image
+import pydeck as pdk
+import assemble_data
+
+fire_point = assemble_data.read_fire_disturbance_point()
+fire_area = assemble_data.read_fire_disturbance_area()
+
+img = Image.open('forest_fire2.jpg')
+st.image(img, use_column_width=True)
+st.title('Abstract')
+
+st.write('''Forest fires have always posed a major threat to Canadian forests.
+        The National Forestry database estimates that Canada endures on average
+        8000 forest fires per year, which burn through 2.1 million acres of
+        landscape \cite{cite_key2}. These fires have become especially
+        concerning within the past century, as the Canadian population has
+        boomed and rural areas have become popular destinations for both home
+        owners and cottagers. The looming threat of climate change has brought
+        an even greater uncertainty to already frightening threat of forest
+        fires in Canada, and it is logical to assume that hotter and drier
+        weather increases the risk of forest fires occurring. Also, The
+        National Forestry database reports that forest fires that begin as a
+        result of a lightning strikes account for 85% of all land burned by
+        fires \cite{cite_key2}. Thus, if climate change were to impact either
+        the frequency of lightning storms, or increase the severity of
+        droughts, forest fires could be impacted greatly. This project will
+        determine if and how climate change affects forest fires in Ontario. We
+        have procured a data set containing forest fire data for Ontario dating
+        back to 2003. Our goal is to analyze how changes in weather patterns
+        have affected the number and size of forest fires in Ontario.''')
+
+st.title('Preliminary Data Analysis')
+
+st.write('''The following map displays data for fires in ontario between 1998
+to 2020 that are under 40 hectares in size. Areas in red are where fires are
+more likely to occur. These fires are more likely to occur in the
+southern-mid section of Ontario, but is still quite frequent throughout the
+non-northern parts of the province.''')
+st.pydeck_chart(pdk.Deck(
+    map_style='mapbox://styles/mapbox/light-v9',
+    initial_view_state=pdk.ViewState(
+        latitude=50,
+        longitude=-86,
+        zoom=4,
+        pitch=30
+    ),
+    layers=[
+        pdk.Layer(
+            "HeatmapLayer",
+            data=fire_point,
+            opacity=0.3,
+            get_position='[longitude, latitude]'
+        )
+        #  pdk.Layer(
+        #     'HexagonLayer',
+        #     data=map_data.fires,
+        #     radius=8000,
+        #     get_position='[longitude, latitude]',
+        #     elevation_scale=30,
+        #     elevation_range=[0, 1000],
+        #     extruded=True
+        #  ),
+        # pdk.Layer(
+        #     "ContourLayer",
+        #     data=map_data.fires,
+        #     get_position='[longitude, latitude]',
+        #     cell_size=8000
+
+        # )
+    ]
+))
+
+st.write('''The following map displays the location of forest fires in
+Ontario from 1998 to 2020 that were greater than 40 hectares in size. Areas
+highlighted in red indicate areas where these fires most frequently occur. 
+The importance of separating fires by size is clearly illustrated by the 
+discrepancy between the two maps. In the map bellow, we can see that larger forest
+fires tend to occur in the north western region of Ontario, while the map above
+shows that smaller fires most often occur further south.''')
+st.pydeck_chart(pdk.Deck(
+    map_style='mapbox://styles/mapbox/light-v9',
+    initial_view_state=pdk.ViewState(
+        latitude=50,
+        longitude=-86,
+        zoom=4,
+        pitch=30
+    ),
+    layers=[
+        pdk.Layer(
+            "HeatmapLayer",
+            data=fire_area,
+            opacity=0.3,
+            get_position='[LONGITUDE, LATITUDE]'
+        )
+        #  pdk.Layer(
+        #     'HexagonLayer',
+        #     data=map_data.fires,
+        #     radius=8000,
+        #     get_position='[longitude, latitude]',
+        #     elevation_scale=30,
+        #     elevation_range=[0, 1000],
+        #     extruded=True
+        #  ),
+        # pdk.Layer(
+        #     "ContourLayer",
+        #     data=map_data.fires,
+        #     get_position='[longitude, latitude]',
+        #     cell_size=8000
+
+        # )
+    ]
+))
+
+st.write('''The data sets we aquired have a few built in metrics we can
+compare fire intensity, size, and frequency to. For example, over our
+datasets, the common causes are:''')
+
+st.bar_chart(fire_area[''])
